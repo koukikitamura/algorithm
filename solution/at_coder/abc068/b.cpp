113ce3a88438ea464d803649f9404ae2572b3b9d
kouki kitamura <developer@koukikitamura.jp>
2020-07-24 22:56:57 +0900

    Solve at_coder/abc068/b

---
+++ solution/at_coder/abc068/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i, to) for (int i = 0; i < (to); ++i)
+#define repf(i, from, to) for (int i = (from); i < (to); ++i)
+#define unless(cond) if (!(cond))
+using namespace std;
+using ll = long long;
+template <typename T>
+using V = vector<T>;
+template <typename T, typename U>
+using P = pair<T, U>;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int power = log2(n);
+  int ans = 1;
+  rep(i, power) {
+    ans *= 2;
+  }
+
+  cout << ans << endl;
+}
