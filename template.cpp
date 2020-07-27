17ff6842f1d66566eb4d8126665e5ce556f612c0
kouki kitamura <developer@koukikitamura.jp>
2020-07-28 00:25:22 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 #define rep(i, to) for (int i = 0; i < (to); i++)
-#define rrep(i, to) for (int i = to - 1; i >= 0; i--)
+#define rrep(i, to) for (int i = (to) - 1; i >= 0; i--)
 #define repf(i, from, to) for (int i = (from); i < (to); i++)
+#define all(v) v.begin(), v.end()
 #define unless(cond) if (!(cond))
 using namespace std;
 using ll = long long;
